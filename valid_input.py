5c341ccd0f7ea885dfb44d1cb659ab5cd26c8c00
Varun Goel <89370605+Toa-st@users.noreply.github.com>
2022-07-25 06:55:25 +1200

    changed file structure

---
+++ valid_input.py
@@ -0,0 +1,29 @@
+def list_valid_input(input_message, error_message, valid_entries):
+    """This function gets valid string input that is in a list (valid_entries)"""
+    user_input = ''
+    
+    while not user_input in valid_entries:
+        user_input = input(input_message).lower()
+        
+        if not (user_input in valid_entries):
+            print(error_message)
+    
+    return user_input
+
+def int_valid_input(input_message, error_message, low, high):
+    """This function gets valid int input that is between two numbers (high and low)"""
+    is_valid = False
+    
+    while not is_valid:
+        try:
+            user_input = int(input(input_message))
+            if  (user_input >= low and user_input <= high):
+                is_valid = True
+            
+            else:
+                print(error_message)
+        
+        except ValueError:
+            print(error_message)
+    
+    return user_input
